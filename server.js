f3a84deb01fe615420696ca247c387d1f2c0900f
karishma <karishma.auzmor.com>
2018-08-27 12:11:35 +0530

    concurrent merge 2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
